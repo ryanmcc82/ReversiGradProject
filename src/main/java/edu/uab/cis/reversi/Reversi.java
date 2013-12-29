72025880a2d4096496aedb7ce96ff3ef2f782d45
Steven Bethard <steven.bethard@gmail.com>
2013-12-29 16:23:10 -0600

    Revises Reversi to be more testable

--- src/main/java/edu/uab/cis/reversi/Reversi.java
+++ src/main/java/edu/uab/cis/reversi/Reversi.java
@@ -3,8 +3,6 @@ package edu.uab.cis.reversi;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.google.common.collect.HashMultiset;
-import com.google.common.collect.Multiset;
 import com.lexicalscope.jewel.cli.CliFactory;
 import com.lexicalscope.jewel.cli.Option;
 
@@ -16,33 +14,47 @@ public class Reversi {
 
     @Option(longName = "strategy2")
     public String getStrategy2();
+
+    @Option(longName = "games")
+    public int getNumberOfGames();
   }
 
-  public static void main(String[] args) {
+  public static void main(String... args) throws Exception {
     Options options = CliFactory.parseArguments(Options.class, args);
-    Strategy strategy1 = loadStrategy(options.getStrategy1());
-    Strategy strategy2 = loadStrategy(options.getStrategy2());
-    Multiset<Strategy> wins = HashMultiset.create();
-    Board board = new Board();
-    for (int i = 0; i < 500; ++i) {
-      Reversi reversi;
-      reversi = new Reversi(strategy1, strategy2);
-      wins.add(reversi.getWinner(reversi.play(board)));
-      reversi = new Reversi(strategy2, strategy1);
-      wins.add(reversi.getWinner(reversi.play(board)));
-    }
-    System.err.printf("Strategy 1: %4d\n", wins.count(strategy1));
-    System.err.printf("Strategy 2: %4d\n", wins.count(strategy2));
-    System.err.printf("Ties:       %4d\n", wins.count(null));
+    Strategy strategy1, strategy2;
+    strategy1 = Class.forName(options.getStrategy1()).asSubclass(Strategy.class).newInstance();
+    strategy2 = Class.forName(options.getStrategy2()).asSubclass(Strategy.class).newInstance();
+    int nGames = options.getNumberOfGames();
+    Map<Strategy, Integer> wins = playMultiple(new Board(), strategy1, strategy2, nGames);
+    System.err.printf(
+        "Strategy 1: %4d\nStrategy 2: %4d\nTies:       %4d\n",
+        wins.get(strategy1),
+        wins.get(strategy2),
+        wins.get(null));
   }
 
-  private static Strategy loadStrategy(String className) {
-    try {
-      Class<?> strategyClass = Class.forName(className);
-      return strategyClass.asSubclass(Strategy.class).newInstance();
-    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
-      throw new RuntimeException(e);
+  public static Map<Strategy, Integer> playMultiple(
+      Board board,
+      Strategy strategy1,
+      Strategy strategy2,
+      int nGames) {
+    Map<Strategy, Integer> wins = new HashMap<>();
+    wins.put(strategy1, 0);
+    wins.put(strategy2, 0);
+    wins.put(null, 0);
+    for (int i = 0; i < nGames / 2; ++i) {
+      Reversi reversi;
+      Strategy winner;
+      // strategy1=BLACK, strategy2=WHITE
+      reversi = new Reversi(strategy1, strategy2);
+      winner = reversi.getWinner(reversi.play(board));
+      wins.put(winner, wins.get(winner) + 1);
+      // strategy2=BLACK, strategy1=WHITE
+      reversi = new Reversi(strategy2, strategy1);
+      winner = reversi.getWinner(reversi.play(board));
+      wins.put(winner, wins.get(winner) + 1);
     }
+    return wins;
   }
 
   private Map<Player, Strategy> strategies;
---
+++ src/test/java/edu/uab/cis/reversi/ReversiTest.java
@@ -0,0 +1,66 @@
+package edu.uab.cis.reversi;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Ordering;
+
+public class ReversiTest {
+
+  static class SequentialStrategy implements Strategy {
+
+    @Override
+    public Square chooseSquare(Board board) {
+      Set<Square> squares = board.getCurrentPossibleSquares();
+      Ordering<Square> byPosition =
+          Ordering.natural().<Integer> lexicographical().onResultOf(
+              new Function<Square, List<Integer>>() {
+                @Override
+                public List<Integer> apply(Square input) {
+                  return Arrays.asList(input.getRow(), input.getColumn());
+                }
+              });
+      return byPosition.min(squares);
+    }
+  }
+
+  @Test
+  public void testPlay() {
+    Strategy blackStrategy = new SequentialStrategy();
+    Strategy whiteStrategy = new SequentialStrategy();
+    Reversi reversi = new Reversi(blackStrategy, whiteStrategy);
+    Board board = reversi.play(new Board());
+    Assert.assertTrue(board.isComplete());
+    Assert.assertEquals(
+        "WWWWWWWB\nWWWWWWBB\nWWWWWBWB\nWWWWBWWB\nWWWWWWWB\nWWWBWWWB\nWWWWBBWB\nBBBBBBWW\n",
+        board.toString());
+    Assert.assertEquals(Player.WHITE, board.getWinner());
+    Assert.assertEquals(whiteStrategy, reversi.getWinner(board));
+  }
+
+  @Test
+  public void testPlayMultiple() {
+    Strategy strategy1 = new SequentialStrategy();
+    Strategy strategy2 = new SequentialStrategy();
+    Map<Strategy, Integer> wins = Reversi.playMultiple(new Board(), strategy1, strategy2, 10);
+    Map<Strategy, Integer> expected = new HashMap<>();
+    expected.put(strategy1, 5);
+    expected.put(strategy2, 5);
+    expected.put(null, 0);
+    Assert.assertEquals(expected, wins);
+  }
+
+  @Test
+  public void testMain() throws Exception {
+    // not a thorough test, but at least we ensure it throws no exceptions
+    String strategy = SequentialStrategy.class.getName();
+    Reversi.main("--strategy1", strategy, "--strategy2", strategy, "--games", "4");
+  }
+}
