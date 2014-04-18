a1693895bb274b6d80c93139db84998ec402b62c
Steven Bethard <steven.bethard@gmail.com>
2014-04-18 08:49:40 -0500

    Updates merge script to better handle resolvable conflicts

--- merge.sh
+++ merge.sh
@@ -5,9 +5,12 @@ artifact_id=reversi
 for username in "$@"; do
   echo "Fetching $username's fork"
   git fetch git@git.cis.uab.edu:$username/$artifact_id.git
-  git diff master FETCH_HEAD
+  git diff ...FETCH_HEAD
+
 
   read -p "Hit enter when ready to merge $username's fork"
+  git stash
   git merge --no-commit --squash FETCH_HEAD
+  git stash pop
 done
 
