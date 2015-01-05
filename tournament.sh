3fa3d6bba5e527285074911e5b0195e095d0e2c1
Steven Bethard <steven.bethard@gmail.com>
2015-01-05 12:48:48 -0500

    Improves how tournament.sh finds baseline strategies

--- tournament.sh
+++ tournament.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -x
 
+baseline_strategies=$(find src/main/java/edu/uab/cis/reversi/strategy/baseline -name "*Strategy.java" | \
+                   sed -e 's/\//./g' -e 's/src.main.java.//g' -e 's/.java//g' | \
+                   tr '\n' ' ')
 group_strategies=$(find src/main/java -name "Group*Strategy.java" | \
                    sed -e 's/\//./g' -e 's/src.main.java.//g' -e 's/.java//g' | \
                    tr '\n' ' ')
 mvn exec:java \
 -Dexec.mainClass="edu.uab.cis.reversi.Reversi" \
 -Dexec.args="--games 10 --timeout 1000 --strategies \
-edu.uab.cis.reversi.strategy.baseline.RandomStrategy \
-edu.uab.cis.reversi.strategy.baseline.PositionalStrategy \
-edu.uab.cis.reversi.strategy.baseline.PreferCornersStrategy \
+$baseline_strategies \
 $group_strategies"
