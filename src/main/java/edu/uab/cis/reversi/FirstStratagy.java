2799105fe05a331015eb18e78540760df3a1039f
Ryan <ryanmcc@uab.edu>
2016-02-29 17:56:14 -0600

    Starting a first strategy

---
+++ src/main/java/edu/uab/cis/reversi/FirstStratagy.java
@@ -0,0 +1,11 @@
+package edu.uab.cis.reversi;
+
+public class FirstStratagy implements Strategy {
+
+    @Override
+    public Square chooseSquare(Board board) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
