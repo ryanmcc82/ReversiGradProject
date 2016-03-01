5cb8ef34d8c2f5ec10d565c4346e830b0838545b
Ryan <ryanmcc@uab.edu>
2016-02-29 19:04:13 -0600

    Huristic framework

---
+++ src/main/java/edu/uab/cis/reversi/FirstHuristic.java
@@ -0,0 +1,35 @@
+package edu.uab.cis.reversi;
+
+public class FirstHuristic {
+    private final boolean TESTPRINTING = false;
+    
+    public static int getScore(){
+        int score = 0;
+        
+        return score;
+    }
+
+    public static int getMobilityScore(){
+        int score = 0;
+        
+        return score;
+    }
+    
+    public static int getScoreCount(){
+        int score = 0;
+        
+        return score;
+    }
+    
+    public static int getStablitycount(){
+        int score = 0;
+        
+        return score;
+    }
+    
+    
+    public static void tprint(String s){
+        System.out.println(s);
+    }
+    
+}
