b20ea5ce058d32a7fa12b28caa3e6f0c5fe17571
Ryan <ryanmcc@uab.edu>
2016-03-29 03:07:43 -0500

    Almost finished with valid move search; needs testing

--- src/main/java/edu/uab/cis/reversi/BitBoardNode.java
+++ src/main/java/edu/uab/cis/reversi/BitBoardNode.java
@@ -8,6 +8,155 @@ public class BitBoardNode {
     static final long bitmask = 1;
     public static final boolean TPRINT = true;
 
+    /* Static References */
+    /*********************************************************************************************************************************/
+    // ###################
+    // # O O O O O O O O #
+    // # O X X X X X X O #
+    // # O X X X X X X O #
+    // # O X X X X X X O #
+    // # O X X X X X X O #
+    // # O X X X X X X O #
+    // # O X X X X X X O #
+    // # O O O O O O O O #
+    // ###################
+    public static final long patternEdges;
+    static {
+        Long temp = 0b11111111L | 0b11111111L << (56);
+        for (int i = 1; i < 7; i++) {
+            temp = temp | 0b10000001L << (8 * i);
+        }
+        patternEdges = temp;
+    }
+
+    public static final long positionArray[];
+    static {
+        positionArray = new long[64];
+        for (int i = 0; i < 64; i++) {
+            positionArray[i] = 0b1L << i;
+        }
+    }
+
+    public static final long ajacentArray[];
+    static {
+        ajacentArray = new long[64];
+        for (int i = 0; i < 64; i++) {
+            if ((0b1L << i & patternEdges) == 0) { // if its not on the edge get
+                                                   // all 8 surounding positions
+                ajacentArray[i] = 0b1L << (i + 1) | 0b1L << (i - 1)
+                        | 0b111L << (i + 7) | 0b111L << (i - 9);
+            } else if (i == 0) {// corner
+                ajacentArray[i] = 0b1 << (1 + i) | 0b11L << (8 + i);
+            } else if (i == 7) {// corner
+                ajacentArray[i] = 0b1 << (i - 1) | 0b11L << (6 + i);
+            } else if (i == 63) {// corner
+                ajacentArray[i] = 0b1 << (i - 1) | 0b11L << (i - 9);
+            } else if (i == 56) {// corner
+                ajacentArray[i] = 0b1 << (1 + i) | 0b11L << (8 + i);
+            } else if ((i + 1) % 8 == 0) {// left most column - corners
+                ajacentArray[i] = 0b1L << (i - 1) | 0b11L << (i + 6)
+                        | 0b11L << (i - 9);
+            } else {// right most column - corners
+                ajacentArray[i] = 0b1L << (i + 1) | 0b11L << (i + 7)
+                        | 0b11L << (i - 7);
+            }
+        }
+    }
+
+    public static long getforwardSlantRay(int length) {
+        long temp = 0b1L;
+        long tempH = 0;
+        for (int i = 0; i < length; i++) {
+            tempH = tempH | (temp << (9 * i));
+        }
+        return tempH;
+    }
+
+    public static long getTopforwardSlantRay(int length) {
+        long temp = 0b1L;
+        long tempH = 0;
+        for (int i = 1; i < length + 1; i++) {
+            tempH = tempH | (temp << (63 - (9 * i)));
+        }
+        return tempH;
+    }
+
+    public static long getBackSlantRay(int length) {
+        long temp = 0b1L;
+        long tempH = 0;
+        for (int i = 0; i < (length); i++) {
+            tempH = tempH | (temp << (7 * i));
+        }
+        return tempH;
+    }
+
+    public static long getTopBackSlantRay(int length) {
+        long temp = 0b1L;
+        long tempH = 0;
+        for (int i = 0; i < (length); i++) {
+            tempH = tempH | (temp << (56 - (7 * i)));
+        }
+        return tempH;
+    }
+
+    public static final long vertRay;
+    static {
+        long temp = 0b1L;
+        long tempH = 0;
+        for (int i = 0; i < 8; i++) {
+            tempH = tempH | (temp << (8 * i));
+        }
+        vertRay = tempH;
+    }
+
+    public static long horizontalRay(int length) {
+        long temp = 0b1L;
+        long tempH = 0;
+        for (int i = 1; i < (length + 1); i++) {
+            tempH = tempH | (temp << i);
+        }
+        return tempH;
+    }
+
+    public static int translationArray[] = { 0, 1, 2, -1, -1, -1, -1, -1, 3,
+            -1, 4, -1, -1, -1, -1, -1, 5, 6, 7 };
+
+    public static final long rayArray[][];
+    static {
+        rayArray = new long[64][8];
+        for (int i = 0; i < 64; i++) {
+            int leftRayLength = 7 - (i % 8);
+            int rightRayLength = (i % 8);
+            long leftUp = getforwardSlantRay(leftRayLength) << (i + 9);
+            rayArray[i][7] = leftUp;
+
+            long up = vertRay << (i + 8);
+            rayArray[i][6] = up;
+
+            long rightUp = getBackSlantRay(rightRayLength) << (i + 7);
+            rayArray[i][5] = rightUp;
+
+            long left = horizontalRay(leftRayLength) << i;
+            rayArray[i][4] = left;
+
+            long right = horizontalRay(rightRayLength) << i
+                    - (rightRayLength + 1);
+            rayArray[i][3] = right;
+
+            long leftDown = getTopBackSlantRay(leftRayLength) >>> 63 - i;
+            rayArray[i][2] = leftDown;
+
+            long down = vertRay >>> 64 - i;
+            rayArray[i][1] = down;
+
+            long rightDown = getTopforwardSlantRay(rightRayLength) >>> 63 - i;
+            rayArray[i][0] = rightDown;
+        }
+
+    }
+
+    /*********************************************************************************************************************************/
+
     public static void printBinary(long board) {
         if (TPRINT) {
             // String s = String.format("%063d", Long.toBinaryString(board));
@@ -78,35 +227,77 @@ public class BitBoardNode {
         }
 
     }
-    
-    public void getMoves(Long opponent, Long movers){
-        Long allPossible = -1L;
-        Long occupied = opponent | movers; //keep this allocated
-       Long unoccupied = allPossible ^ occupied;
-       occupied.
-       Long tempOpp = opponent;
-       Long searchBit = Long.highestOneBit(tempOpp);
-       Long surrounding;
-       Long moves = 0L;
-       
-       if(Long.bitCount(occupied) < 32){//Note May should skip this comparison and just default to one or the other
-           while(searchBit != 0L){
-               surrounding = ajacentArray[Long.numberOfTrailingZeros(searchBit)];
-               
-               tempOpp = tempOpp ^ searchBit;
-               searchBit = Long.highestOneBit(tempOpp);
-           }
-       }
-//       int  set = opponent.
-       Long possibles = 
-       Long.highestOneBit(opponent);
+
+    public void moveResult(long move, long opponent, long movers) {
+
+    }
+
+    public long getLegalMoves(long opponent, long movers) {
+        // Later in game its faster to look at just empty spaces.
+        // Early in game might be faster to look at it from occupied spaces. or
+        // weed out based on boarder squares
+        long occupied = opponent | movers; // keep this allocated
+        long unoccupied = ~occupied;
+
+        long tempUnOcc = unoccupied;
+        long searchBit = Long.lowestOneBit(tempUnOcc);
+        long surrounding;
+        long surroundingOpp;
+        long moves = 0L;
+
+        long searchDirBit;
+        int searchDirDiff;
+        int squareIndex;
+        int emptyIndex;
+        int moverIndex;
+        long searchDirRay;
+        long moverRayIntersect;
+
+        // if(Long.bitCount(occupied) < 32){//Note May should skip this
+        // comparison and just default to one or the other
+        while (searchBit != 0L) {
+            surrounding = ajacentArray[Long.numberOfTrailingZeros(searchBit)];// gets surrounding squares from static table
+            surroundingOpp = surrounding & opponent;
+
+            while (surroundingOpp != 0L) {// if none of the surrounding squares are an opponent then its not a valid move
+
+                searchDirBit = Long.lowestOneBit(surroundingOpp);
+                squareIndex = Long.numberOfTrailingZeros(searchBit);
+                searchDirDiff = Long.numberOfTrailingZeros(searchDirBit)
+                        - squareIndex;// This diff lets us search in a diretion using bitshift.
+
+                searchDirRay = rayArray[squareIndex][translationArray[searchDirDiff + 9]];
+                moverRayIntersect = searchDirRay & movers;
+                if(moverRayIntersect!= 0L){// if mover has no pieces in ray path its not valid move.
+                    if(searchDirDiff > 0){
+                        moverIndex = Long.numberOfTrailingZeros(moverRayIntersect);
+                        emptyIndex = Long.numberOfTrailingZeros(searchDirRay & unoccupied);
+                        if(moverIndex < emptyIndex) moves = moves |searchBit;//add square to valid Moves
+                    }else{
+                        moverIndex = Long.numberOfLeadingZeros(moverRayIntersect);
+                        emptyIndex = Long.numberOfLeadingZeros(searchDirRay & unoccupied);
+                        if(moverIndex < emptyIndex) moves = moves |searchBit;//add square to valid Moves
+                    }
+                }
+
+                surroundingOpp = surroundingOpp & ~searchDirBit;// zero's search
+                                                                // Direction
+            }
+
+            tempUnOcc = tempUnOcc ^ searchBit;// sets searched bit to zero
+            searchBit = Long.lowestOneBit(tempUnOcc);// finds new bit(square) to
+                                                     // search.
+        }
+        return moves;
     }
 
     public static void main(String args[]) {
-
+        BitBoardNode  searcher= new BitBoardNode();
         long tMax = -1L;
         long tMin = Long.MIN_VALUE;
-        long test[] = {tMax, (sq11flag | sq00flag | sq22flag | sq33flag | sq44flag), patternEdges };
+        long test[] = {
+                tMax,
+                (sq33flag | sq44flag ), (sq34flag | sq43flag ), searcher.getLegalMoves((sq33flag | sq44flag ),(sq34flag | sq43flag ))};
         int count = 0;
 
         for (long sq : test) {
@@ -120,8 +311,7 @@ public class BitBoardNode {
             printSBoard(sq);
         }
     }
-    
-    
+
     // ###################
     // # X X X X X X X X #
     // # X X X X X X X X #
@@ -206,87 +396,11 @@ public class BitBoardNode {
     public static final long sq03flag = 0b1000000000000000000000000000000000000000000000000000000000000L;
     public static final long sq02flag = 0b10000000000000000000000000000000000000000000000000000000000000L;
     public static final long sq01flag = 0b100000000000000000000000000000000000000000000000000000000000000L;
-    public static final long sq00flag = 0b1000000000000000000000000000000000000000000000000000000000000000L;// Warning this is for sign bit
-  
+    public static final long sq00flag = 0b1000000000000000000000000000000000000000000000000000000000000000L;// Warning
+                                                                                                            // this
+                                                                                                            // is
+                                                                                                            // for
+                                                                                                            // sign
+                                                                                                            // bit
 
-    //###################
-    //# O O O O O O O O #
-    //# O X X X X X X O #
-    //# O X X X X X X O #
-    //# O X X X X X X O #
-    //# O X X X X X X O #
-    //# O X X X X X X O #
-    //# O X X X X X X O #
-    //# O O O O O O O O #
-    //###################
-      public static final long patternEdges;
-//      = (sq01flag
-//              |sq00flag
-//              |sq02flag
-//              |sq03flag
-//              |sq04flag
-//              |sq05flag
-//              |sq06flag
-//              |sq07flag
-//              |sq70flag
-//              |sq71flag
-//              |sq72flag
-//              |sq73flag
-//              |sq74flag
-//              |sq75flag
-//              |sq76flag
-//              |sq77flag
-//              |sq10flag
-//              |sq20flag
-//              |sq30flag
-//              |sq40flag
-//              |sq50flag
-//              |sq60flag
-//              |sq67flag
-//              |sq57flag
-//              |sq47flag
-//              |sq37flag
-//              |sq27flag
-//              |sq17flag
-//              );
-      
-      static {
-          Long temp = 0b11111111L | 0b11111111L<<(56);
-          for(int i = 1 ; i < 7; i++){
-              temp = temp | 0b10000001L<<(8*i);
-          }
-          patternEdges = temp;
-      }
-
-      public static final long positionArray[];
-      static{
-          positionArray = new long[64];
-          for(int i = 0; i<64; i++){
-              positionArray[i]= 0b1L<<i;
-          }
-      }    
-      
-      public static final long ajacentArray[];
-      
-      static {
-          ajacentArray = new long[64];
-          for(int i = 0; i < 64; i++){
-              if((0b1L<<i & patternEdges) == 0){ //if its not on the edge get all 8 surounding positions
-                  ajacentArray[i] = 0b1L<<(i+1) | 0b1L<<(i-1)| 0b111L<<(i+7) | 0b111L<<(i-9);
-              }else if(i==0){//corner
-                  ajacentArray[i]= 0b1<<(1+i) | 0b11L<<(8+i);
-              }else if(i==7){//corner
-                  ajacentArray[i]= 0b1<<(i-1) | 0b11L<<(6+i);
-              }else if(i==63){//corner
-                  ajacentArray[i]= 0b1<<(i-1) | 0b11L<<(i-9);
-              }else if(i==56){//corner
-                  ajacentArray[i]= 0b1<<(1+i) | 0b11L<<(8+i);
-              }else if((i+1)%8 ==0){//left most column - corners
-                  ajacentArray[i]= 0b1L<<(i-1)| 0b11L<<(i+6) | 0b11L<<(i-9);
-              }else {//right most column - corners
-                  ajacentArray[i]= 0b1L<<(i+1)| 0b11L<<(i+7) | 0b11L<<(i-7);
-              }
-          }
-          
-      }
 }
