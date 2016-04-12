1711024211b54c7a68d1739e1b93ffe27266fce9
Ryan <ryanmcc@uab.edu>
2016-04-12 06:13:04 -0500

    book fixes

--- src/main/java/edu/uab/cis/reversi/strategy/group3/Book.java
+++ src/main/java/edu/uab/cis/reversi/strategy/group3/Book.java
@@ -1,98 +1,98 @@
-package reversi;
+package edu.uab.cis.reversi.strategy.group3;
+
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
+
 import edu.uab.cis.reversi.Move;
 import edu.uab.cis.reversi.Player;
 import edu.uab.cis.reversi.Square;
 
-
-
 public class Book {
 
-	private HashMap<List<Move>,Square> map;
-	private Player player1 = Player.BLACK;
+    private HashMap<List<Move>, Square> map;
+    private Player player1 = Player.BLACK;
     private Player player2 = Player.WHITE;
-	public Book(){
-		try {
-			readData();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	
-	
-	private void readData() throws IOException{
-		 try {
-				BufferedReader reader = new BufferedReader(new FileReader("book.txt"));
-				String temp = reader.readLine();
-				while(temp != null){
-					String[] split = temp.split(" ");
-					temp = reader.readLine();
-                    ArrayList<Move> seq = new ArrayList<Move>();
-					for(int i = 1; i <= (split.length/2); i++){
-					if(i < 2){
-						Move move1 = new Move(new Square(Integer.parseInt(split[i-1]),Integer.parseInt(split[i]) ),player1);
-                        seq.add(move1);
-                    }else{
-                    	if(i%2 == 0 ){
-                    		Square thisMove = new Square(Integer.parseInt(split[i*2-1]),Integer.parseInt(split[i*2]) );
-                    		Move move1 = new Move( thisMove,player2);
-                    		if(!map.containsKey((PSequence<Move>) seq)){
-                        		map.put(seq, thisMove);	
-                        		}
-                            	seq.add(move1);
-                    	}else{
-                    			Square thisMove = new Square(Integer.parseInt(split[i*2-1]),Integer.parseInt(split[i*2+1]) );
-                        		Move move1 = new Move(thisMove,player1);
-                        		if(!map.containsKey((PSequence<Move>) seq)){
-                        		map.put(seq, thisMove);	
-                        		}
-                        		seq.add(move1);
-                    	}
-                    	
-                    }
-						
-						
-					}
-					temp = reader.readLine();
-				}
-				
-				reader.close();
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				
-			}
-		 
-		 
-	}
+    private boolean loaded = true;
 
-public Square checkBook(List<Move> history){
-	if(map.containsKey(history)){
-		return map.get(history);
-	}else return null;
-		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+    public Book() {
+        map = new HashMap<List<Move>, Square>();
+        try {
+            readData();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        printBook();
+    }
+    private void printBook(){
+        for( Entry<List<Move>, Square> entry: map.entrySet()){
+            System.out.println(entry);
+        }
+    }
+
+    private void readData() throws IOException {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(
+                    "book.txt"));
+            String temp = reader.readLine();
+            while (temp != null) {
+                String[] split = temp.split(" ");
+                temp = reader.readLine();
+                ArrayList<Move> seq = new ArrayList<Move>();
+                Move move1 = new Move(new Square(
+                        Integer.parseInt(split[0]),
+                        Integer.parseInt(split[1])), player1);
+                seq.add(move1);
+                for (int i = 1; i <= ((split.length - 1) / 2); i++) {
+
+                        if (i % 2 == 1) {
+                            Square thisMove = new Square(
+                                    Integer.parseInt(split[i * 2]),
+                                    Integer.parseInt(split[i * 2 + 1]));
+                            Move moveP2 = new Move(thisMove, player2);
+                            
+                            if (!map.containsKey(seq)) {
+                                map.put(new ArrayList<Move>(seq), thisMove);
+                            }
+                            seq.add(moveP2);
+                        } else {
+                            Square thisMove = new Square(
+                                    
+                                    Integer.parseInt(split[i * 2]),
+                                    Integer.parseInt(split[i * 2 + 1]));
+                            Move moveP1 = new Move(thisMove, player1);
+                            if (!map.containsKey(seq)) {
+                                map.put(new ArrayList<Move>(seq), thisMove);
+                            }
+                            seq.add(moveP1);
+                        }    
+
+                }
+                temp = reader.readLine();
+            }
+
+            reader.close();
+        } catch (FileNotFoundException e) {
+            this.loaded = false;
+            e.printStackTrace();
+
+        }
+
+    }
+
+    public Square checkBook(List<Move> history) {
+
+        if (loaded && map.containsKey(history)) {
+            return map.get(history);
+        } else
+            return null;
+
+    }
+
 }
--- src/main/java/edu/uab/cis/reversi/strategy/group3/FirstStratagy.java
+++ src/main/java/edu/uab/cis/reversi/strategy/group3/FirstStratagy.java
@@ -1,10 +1,12 @@
 package edu.uab.cis.reversi.strategy.group3;
 
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import edu.uab.cis.reversi.Board;
+import edu.uab.cis.reversi.Move;
 import edu.uab.cis.reversi.Square;
 import edu.uab.cis.reversi.Strategy;
 
@@ -13,35 +15,39 @@ public class FirstStratagy implements Strategy {
     private TimeUnit timeunit;
     private Book openingBook;
     private boolean inBook = true;
+    int testcount = 0;
 
     public FirstStratagy() {
         openingBook = new Book();
     }
     @Override
     public Square chooseSquare(Board board) {
+        List<Move> movesList = board.getMoves();
+        if(movesList.size() < 1)inBook = true;
 
         Square move;
         if (inBook) {
-            move = openingBook.checkBook(board.getMoves());
+            move = openingBook.checkBook(movesList);
             if (move == null) {
+                System.out.println("BookDepth: " + testcount);
                 inBook = false;
-            } else
+            } else {
+                testcount++;
                 return move;
-
+            }
         }
         move = Square.PASS;
-        
-        int minMobility =  Integer.MAX_VALUE;
+
+        int minMobility = Integer.MAX_VALUE;
         Set<Square> moves = board.getCurrentPossibleSquares();
-        for(Square moveP : moves){
+        for (Square moveP : moves) {
             int mobility = board.play(moveP).getCurrentPossibleSquares().size();
             if (mobility < minMobility) {
                 move = moveP;
-                minMobility = mobility ;
+                minMobility = mobility;
             }
         }
-       
-        
+
         return move;
     }
     
